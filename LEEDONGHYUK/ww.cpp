f36273fda278686cc555025721d74cc68a642bc6
ewqr9808 <ewqr9808@gmail.com>
2025-04-11 17:18:48 +0900

    second commit
    Change-Id: I68a25e810b2e827b50c69d74b833550c3dbba9fc

--- LEEDONGHYUK/ww.cpp
+++ LEEDONGHYUK/ww.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
 
 int main(){
 
-        std:: cout << "hello world" << std::endl;
+        int N;
+        cin >>N;
+
+        std:: cout << N << std::endl;
         return 0;
 
 }
